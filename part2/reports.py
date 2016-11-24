16f41d37c651b682bed68f54782565c74af55a74
imremeszesan <imre.meszesan@gmail.com>
2016-11-24 08:57:05 +0100

    added sum_sold and get_selling_avg functions

--- part2/reports.py
+++ part2/reports.py
@@ -1,3 +1,4 @@
+import math
 """
 What is the title of the most played game?
 Expected name of the function: get_most_played(file_name)
@@ -7,7 +8,7 @@ Other expectation:  if there is more than one, then return the first from the fi
 def get_most_played(file_name):
 
     game_list = []
-    with open("game_stat.txt", mode="r", encoding="UTF-8") as game_file:
+    with open(file_name, mode="r", encoding="UTF-8") as game_file:
         for line in game_file:
             game_list.append(line.strip().split("\t"))
 
@@ -18,13 +19,22 @@ def get_most_played(file_name):
 
     return max[0]
 
-
-
 """
 How many copies have been sold total?
 Expected name of the function: sum_sold(file_name)
 Expected output of the function: (number)
 """
+
+def sum_sold(file_name):
+
+    game_list = []
+    with open(file_name, mode="r", encoding= "UTF-8") as game_file:
+        for line in game_file:
+            game_list.append(line.strip().split("\t"))
+
+    return round(sum(float(game[1]) for game in game_list), 2)
+
+
 
 """
 What is the average selling?
@@ -32,6 +42,23 @@ Expected name of the function: get_selling_avg(file_name)
 Expected output of the function: (number)
 Other expectation: if there is more than one, then return the first from the file
 """
+
+def get_selling_avg(file_name):
+
+    game_list = []
+    with open(file_name, mode="r", encoding= "UTF-8") as game_file:
+        for line in game_file:
+            game_list.append(line.strip().split("\t"))
+
+    
+
+    for game_index1 in range(len(game_list)): 
+        for game_index2 in range(game_index1+1, len(game_list)-1): 
+            if game_list[game_index1][0] == game_list[game_index2][0]:
+                game_list.pop(game_index2)
+
+    return round((sum(float(game[1]) for game in game_list) / len(game_list)), 2)
+
 
 """
 How many characters long is the longest title?
