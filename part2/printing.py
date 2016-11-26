b24113f6435a4bb9f8d57c0f5b6a8d15311788a4
imremeszesan <imre.meszesan@gmail.com>
2016-11-26 08:38:59 +0100

    initial commit

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,10 @@
+from reports import *
 
-# Printing functions
+print("Most played game: ", get_most_played("game_stat.txt"))
+print("\nCopies sold total: ", sum_sold("game_stat.txt"))
+print("\nSellling avarage: ", get_selling_avg("game_stat.txt"))
+print("\nLongest title: ", count_longest_title("game_stat.txt"))
+print("\nAvarage release date: ", get_date_avg("game_stat.txt"))
+print("\nProperties of x game: \n", get_game("game_stat.txt", "Minecraft"))
+print("\nGames in a genre: \n", count_grouped_by_genre("game_stat.txt"))
+print("\nDate ordered list of games: \n", get_date_ordered("game_stat.txt"))
