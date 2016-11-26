2b3c88952bfdcbb72ba387d7bb78448adc64cda8
imremeszesan <imre.meszesan@gmail.com>
2016-11-26 08:30:38 +0100

    export using dir() and eval()

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,11 @@
+import reports
 
-# Export functions
+title = input("Enter a title: ")
+with open("reports.txt", mode="w", encoding="UTF-8") as export_file:
+    for method in dir(reports):
+        if not method.startswith('__'):
+            try:
+                export_file.write(str(eval('reports.' + method + '("game_stat.txt")')) + '\n')
+            except TypeError:
+                export_file.write(str(eval('reports.' + method + '("game_stat.txt"'+ ', "' + title + '")')) + '\n')
+
